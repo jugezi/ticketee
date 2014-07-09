04f81c89e6adf242ffe1c93d1cd70a71a2d77876
Jukka Kalliolehto <jukka.kalliolehto@hotmail.fi>
2014-07-10 02:19:50 +0300

    Restrict access to project actions to admins only

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -6,8 +6,18 @@ module AuthenticationHelpers
     click_button "Sign in"
     expect(page).to have_content("Signed in successfully.")
   end
+end
+
+module AuthHelpers
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
 end 
 
 RSpec.configure do |c|
   c.include AuthenticationHelpers, type: :feature
+end
+
+RSpec.configure do |c|
+  c.include AuthHelpers, type: :controller
 end
